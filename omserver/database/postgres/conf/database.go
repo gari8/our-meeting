d372e74ec0ed62763a3535ef7a0783cc31541fae
gari8 <hagari.hayato.infratop@gmail.com>
2020-12-27 14:24:24 +0900

    add : heroku.yml

--- omserver/database/postgres/conf/database.go
+++ omserver/database/postgres/conf/database.go
@@ -2,18 +2,13 @@ package conf
 
 import (
 	"database/sql"
-	"fmt"
 	_ "github.com/lib/pq"
 	"os"
 )
 
 var (
 	DRIVER   = os.Getenv("DRIVER")
-	HOSTNAME = os.Getenv("HOSTNAME")
-	USER     = os.Getenv("USER")
-	DBNAME   = os.Getenv("DBNAME")
-	PASSWORD = os.Getenv("PASSWORD")
-	source   = fmt.Sprintf("host=%s user=%s dbname=%s password=%s sslmode=disable", HOSTNAME, USER, DBNAME, PASSWORD)
+	source = os.Getenv("DATABASE_URL")
 )
 
 func NewDatabaseConnection() (*sql.DB, error) {
