c29d996038ee6113145b357738390e3e1267f98a
gari8 <hagari.hayato.infratop@gmail.com>
2021-02-02 02:22:18 +0900

    change

--- omclient/src/lib/applicationClient.tsx
+++ omclient/src/lib/applicationClient.tsx
@@ -13,10 +13,7 @@ function createApolloClient() {
         ssrMode: typeof window === 'undefined',
         link: new HttpLink({
             uri: process.env.NEXT_PUBLIC_API_URL + '/query', // Server URL (must be absolute)
-            fetchOptions: {
-                mode: 'no-cors'
-            }
-            // credentials: 'include', // Additional fetch() options like `credentials` or `headers`
+            credentials: 'same-origin', // Additional fetch() options like `credentials` or `headers`
         }),
         cache: new InMemoryCache({
             typePolicies: {
